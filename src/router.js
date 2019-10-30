f9b2b313862a60b10d590e586e03b1bd4d2a39fe
Oleg <filinskyi@gmail.com>
2019-10-30 23:48:52 +0200

    Protected routes from unauthorized users

--- src/router.js
+++ src/router.js
@@ -1,17 +1,12 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from "vue"
+import Router from "vue-router"
+import firebase from "firebase/app"
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   routes: [
-    {
-      path: "/",
-      name: "home",
-      meta: { layout: "main" },
-      component: () => import("./views/Home.vue")
-    },
     {
       path: "/login",
       name: "login",
@@ -21,44 +16,67 @@ export default new Router({
     {
       path: "/register",
       name: "register",
-      meta: { layout: "auth" },
+      meta: { layout: "auth", auth: true },
       component: () => import("./views/Register.vue")
+    },
+    {
+      path: "/",
+      name: "home",
+      meta: { layout: "main", auth: true },
+      component: () => import("./views/Home.vue")
     },
     {
       path: "/categories",
       name: "categories",
-      meta: { layout: "main" },
+      meta: { layout: "main", auth: true },
       component: () => import("./views/Categories.vue")
     },
     {
       path: "/history",
       name: "history",
-      meta: { layout: "main" },
+      meta: { layout: "main", auth: true },
       component: () => import("./views/History.vue")
     },
     {
       path: "/detail-record",
       name: "detail-record",
-      meta: { layout: "main" },
+      meta: { layout: "main", auth: true },
       component: () => import("./views/DetailRecord.vue")
     },
     {
       path: "/planning",
       name: "planning",
-      meta: { layout: "main" },
+      meta: { layout: "main", auth: true },
       component: () => import("./views/Planning.vue")
     },
     {
       path: "/profile",
       name: "profile",
-      meta: { layout: "main" },
+      meta: { layout: "main", auth: true },
       component: () => import("./views/Profile.vue")
     },
     {
       path: "/record",
       name: "record",
-      meta: { layout: "main" },
+      meta: { layout: "main", auth: true },
       component: () => import("./views/Record.vue")
     }
   ]
-});
+})
+
+// for protect routes
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser
+  const requireAuth = to.matched.some(record => record.meta.auth)  // matched - [array]
+
+  if (requireAuth && !currentUser) {
+    next('/login?message=login')  // redirect to the login page
+  } else if (to.path === '/login' && currentUser) {
+    next('/') // for authorized users did not get to the login page
+  } else {
+    next() // redirect to the next page
+  }
+})
+
+export default router
