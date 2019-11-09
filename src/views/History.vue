91a71f8f0590f11c2bd07ee5068cffd7b6afffd6
Oleg <filinskyi@gmail.com>
2019-11-09 21:54:23 +0200

    Created chart with vue-chartjs on History page

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,8 @@
     </div>
 
     <div class="history-chart">
-      <canvas></canvas>
+      <!--Canvas need ref 'canvas' for init, have error without it-->
+      <canvas ref="canvas"></canvas>
     </div>
 
     <Loader v-if="loading" />
@@ -37,10 +38,12 @@
 <script>
   import paginationMixin from '@/mixins/pagination.mixin' // include fields: page, pageSize, pageCount, allItems, items, setupPagination
   import HistoryTable from '@/components/HistoryTable'
+  import { Pie } from 'vue-chartjs'
 
   export default {
     name: 'history',
-    mixins: [paginationMixin],
+    extends: Pie, // renderChart method
+    mixins: [paginationMixin], // setupPagination method
     data: () => ({
       loading: true,
       records: []
@@ -49,18 +52,55 @@
       this.records = await this.$store.dispatch('fetchRecords')
       const categories = await this.$store.dispatch('fetchCategories')
 
-      const recordsWithAdditionalFields = this.records.map(record => {  // transform records
-        return {
-          ...record,
-          categoryName: categories.find(cat => cat.id === record.categoryId).title,  // find category title
-          typeClass: record.type === 'income' ? 'green' : 'red',
-          typeText: record.type === 'income' ? 'Доход' : 'Расход',
-        }
+      this.setup(categories)
+
+      this.renderChart({
+        labels: categories.map(cat => cat.title),
+        datasets: [{
+          label: 'Расходы по категориям',
+          data: categories.map(cat => {
+            return this.records.reduce((total, rec) => { // iterate over records related costs in this category, and summarize them
+              if (rec.categoryId === cat.id && rec.type === 'outcome') {
+                total += +rec.amount
+              }
+              return total
+            }, 0)
+          }),
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
       })
 
-      this.setupPagination(recordsWithAdditionalFields)
-
       this.loading = false
+    },
+    methods: {
+      setup(categories) {
+        const recordsWithAdditionalFields = this.records.map(record => {  // transform records
+          return {
+            ...record,
+            categoryName: categories.find(cat => cat.id === record.categoryId).title,  // find category title
+            typeClass: record.type === 'income' ? 'green' : 'red',
+            typeText: record.type === 'income' ? 'Доход' : 'Расход',
+          }
+        })
+
+        this.setupPagination(recordsWithAdditionalFields)
+      }
     },
     components: {
       HistoryTable
