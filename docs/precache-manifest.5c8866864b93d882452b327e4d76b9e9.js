6ca9ef01780febf1069c41ba6bfa2c4d422df243
Oleg <filinskyi@gmail.com>
2019-11-10 22:32:14 +0200

    Created docs folder for the GitHub pages

---
+++ docs/precache-manifest.5c8866864b93d882452b327e4d76b9e9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "abb45cb97b36a838b626",
+    "url": "/js/chunk-3a17c891.9f013635.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "218423e64852b1bfd409",
+    "url": "/js/chunk-3be11118.9f12b265.js"
+  },
+  {
+    "revision": "4e1a89b2b47cad7973f2",
+    "url": "/css/chunk-0b96928a.8366d153.css"
+  },
+  {
+    "revision": "9a8ecbe3eaef3ba6fe8a",
+    "url": "/css/chunk-1b3eeee0.d7011186.css"
+  },
+  {
+    "revision": "9a8ecbe3eaef3ba6fe8a",
+    "url": "/js/chunk-1b3eeee0.4332a5f2.js"
+  },
+  {
+    "revision": "3318ab1582fd94e1c131",
+    "url": "/js/chunk-2d0d7699.bee52a1f.js"
+  },
+  {
+    "revision": "cfa3dff299d6d65e6cba",
+    "url": "/js/chunk-2d21a3d2.6cbbadec.js"
+  },
+  {
+    "revision": "abb45cb97b36a838b626",
+    "url": "/css/chunk-3a17c891.dcb7b9f0.css"
+  },
+  {
+    "revision": "b526fb03e4e23fa102d4",
+    "url": "/js/app.aba9801c.js"
+  },
+  {
+    "revision": "4e1a89b2b47cad7973f2",
+    "url": "/js/chunk-0b96928a.ed00d983.js"
+  },
+  {
+    "revision": "d02db80c48cb3d15adec",
+    "url": "/js/chunk-4e668e14.5ccdc05a.js"
+  },
+  {
+    "revision": "2cf1562e6e740d074745",
+    "url": "/js/chunk-5f4ad635.2011873a.js"
+  },
+  {
+    "revision": "8dd0c6a8d82c317883e8",
+    "url": "/css/chunk-7f363ba7.44eb83e8.css"
+  },
+  {
+    "revision": "8dd0c6a8d82c317883e8",
+    "url": "/js/chunk-7f363ba7.d1d3c5fc.js"
+  },
+  {
+    "revision": "63f8ecbf0a7d5bee5527",
+    "url": "/css/chunk-vendors.5abecdc7.css"
+  },
+  {
+    "revision": "63f8ecbf0a7d5bee5527",
+    "url": "/js/chunk-vendors.ca009ec1.js"
+  },
+  {
+    "revision": "eff5adc9174c1163f20044775ec53149",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b526fb03e4e23fa102d4",
+    "url": "/css/app.ec993ef9.css"
+  }
+];
